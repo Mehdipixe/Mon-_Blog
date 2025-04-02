19da0a4accc46ee3d1425d1b320e2e9d38a8b6cf
[Mehdipixe] <[mehdiahnou@gmail.com]>
2025-04-02 11:53:20 +0200

    first commit

---
+++ MVC.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS mon_blog  CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE mon_blog;
+
+
+CREATE TABLE articles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titre VARCHAR(255) NOT NULL,
+    contenu TEXT NOT NULL,
+    date_creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
+
+INSERT INTO articles(titre,contenu) VALUES
+('Premier article', 'Ceci est le contenu de mon premier article'),
+('Deuxième article','Et voici le contenu de mon deuxième article');
